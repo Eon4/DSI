553cd03d760a48c7e1e464882145256286e835c3
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-06-07 11:13:16 +0200

    update

--- Sites/HF - Juni 2023/Routes/post.router.js
+++ Sites/HF - Juni 2023/Routes/post.router.js
@@ -1,13 +1,46 @@
-import express from 'express'
+// import express from 'express'
 
-const postRouter = express.Router()
+// const postRouter = express.Router()
 
-postRouter.get('/posts', (req, res) => {
-	res.send('Hent alle poster')
-})
-postRouter.post('/posts', (req, res) => {
-	console.log(req.body);
-	res.send('Opret ny post')
-})
+// postRouter.get('/posts', (req, res) => {
+// 	res.send('Hent alle poster')
+// })
+// postRouter.post('/posts', (req, res) => {
+// 	console.log(req.body);
+// 	res.send('Opret ny post')
+// })
 
-export { postRouter }
+// export { postRouter }
+
+import express from "express";
+// import db from "./Config/db.config.js";
+
+const postRouter = express.Router();
+
+postRouter.get("/", (req, res) => {
+  console.log(req.query);
+  res.send("Hent alle posts");
+});
+
+postRouter.post("/", (req, res) => {
+  res.send(`${req.body.firstname} ${req.body.lastname}
+  ${req.body.email}
+  ${req.body.birthday}
+  ${req.body.password}
+  ${req.body.job}
+  ${req.body.address}
+  
+  
+  
+  `);
+});
+
+postRouter.put("/", (req, res) => {
+  res.send("Opdate post");
+});
+
+postRouter.delete("/", (req, res) => {
+  res.send("Sletter post");
+});
+
+export { postRouter };
